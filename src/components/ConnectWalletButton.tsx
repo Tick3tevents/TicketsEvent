d96dfc79dc5dda94b3bc8058d06d1e2015db00c2
Tick3tevents <tick3tevents@gmail.com>
2025-05-25 18:09:15 +0300

    Delete src directory

--- src/components/ConnectWalletButton.tsx
+++
@@ -1,8 +0,0 @@
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import '@solana/wallet-adapter-react-ui/styles.css';
-
-export default function ConnectWalletButton() {
-  return (
-    <WalletMultiButton className="!bg-indigo-600 !text-white !rounded-lg" />
-  );
-}
